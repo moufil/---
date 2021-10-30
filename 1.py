1ccb7f3b97da45c7a06570a0b5c68aa032097a5c
moufil <90512676+moufil@users.noreply.github.com>
2021-10-30 18:22:02 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,86 +1,83 @@
 import cards, games
+
 class BJ_Card(cards.Positionable_Card):
-    """Карта для игры в Блэк Джэк"""
-    ACE_VALUE=1
-    @property  
+    ACE_VALUE = 1
+    @property
     def value(self):
         if self.is_face_up:
-            v=BJ_Card.Ranks.index(self.rank)+1
-            if v>10:
-                v=10
+            v = BJ_Card.RANKS.index(self.rank) + 1
+            if v > 10:
+                v = 10
         else:
-            v=None
+            v = None
         return v
 class BJ_Deck(cards.Deck):
-    """колода для игры в Блэк Джэк"""
-    def populate(self):
-        for suit in BJ_Card.Suits:
-            for rank in BJ_Card.Ranks:
-                self.cards.append(BJ_Card(rank,suit))
+    """ Black Jack Deck"""
+    def populate(self):        
+        for suit in BJ_Card.SUITS:
+            for rank in BJ_Card.RANKS:
+                self.cards.append(BJ_Card(rank, suit))
 class BJ_Hand(cards.Hand):
-    """Рука игрока в Блэк Джэк"""
-    def init(self,name):
-        super().init()
-        self.name=name
-    def str(self):
-        rep=self.name+":\t"+super().str()
+    """BlackJack player hand"""
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+    def __str__(self):
+        rep = self.name + ":\t" + super().__str__()
         if self.total:
-            rep+="("+str(self.total)+")"
-        return rep
-    @property  
+            rep += "(" + str(self.total) + ")"
+        return rep  
+    @property
     def total(self):
         for card in self.cards:
             if not card.value:
                 return None
-        t=0
-        contains_ace=False
+        t = 0
+        contains_ace = False
         for card in self.cards:
-            t+=card.value
-            if card.value==BJ_Card.ACE_VALUE:
-                contains_ace=True
-        if contains_ace and t<11:
-            t+=10
+            t += card.value
+            if card.value == BJ_Card.ACE_VALUE:
+                contains_ace = True
+        if contains_ace and t <= 11:
+            t += 10
         return t
     def is_busted(self):
-        return self.total>21
+        return self.total > 21
 class BJ_Player(BJ_Hand):
-    """Игрок в блэк джэк"""
     def is_hitting(self):
-        response=games.ask_yes_no("\n"+self.name+
-        ",будете брать ещё карты")
-        return response=="y"
+        response = games.ask_yes_no("\n" + self.name + ", take more cards")
+        return response == "y"
     def bust(self):
-        print(self.name,"пребрал(а).")
+        print(self.name, "Took too much.")
         self.lose()
     def lose(self):
-        print(self.name, "проиграл(а).")
+        print(self.name, " is defeated.")
     def win(self):
-        print(self.name,"выиграл(а).")
+        print(self.name, " has won!")
     def push(self):
-        print(self.name,"сыграл(а) сдиллером в ничью")
+        print(self.name, " pushed.")
 class BJ_Dealer(BJ_Hand):
-    """Диллер в Блэк Джэк"""
     def is_hitting(self):
-        return self.total<17
+        return self.total < 17
     def bust(self):
-        print(self.name,"перебрал(а).")
+        print(self.name, " took too much")
     def flip_first_card(self):
-        first_card=self.cards[0]
+        first_card = self.cards[0]
         first_card.flip()
 class BJ_Game:
-    """Игра в Блэк Джэк"""
-    def init(self,names):
-        self.players=[]
+    def __init__(self, names):
+        self.players = []
         for name in names:
-            player=BJ_Player(name)
+            player = BJ_Player(name)
             self.players.append(player)
-        self.dealer=BJ_Dealer("Диллер")
-        self.deck=BJ_Deck()
-        self.deck.populate()
+        self.dealer = BJ_Dealer("Dealer")
+        self.deck = BJ_Deck()
+        for i in range(4):
+            self.deck.populate()
         self.deck.shuffle()
-    @property  
+    @property
     def still_playing(self):
-        sp=[]
+        sp = []
         for player in self.players:
             if not player.is_busted():
                 sp.append(player)
@@ -93,12 +90,11 @@ class BJ_Game:
                 player.bust()
     def play(self):
         count = len(self.deck.cards)
-        if count > 156:
+        if count < 156:
             self.deck.clear()
             self.deck.populate()
-            self.deck.shuffle()            
-        self.deck.deal(self.players+[self.dealer],
-            per_hand=2)
+            self.deck.shuffle()
+        self.deck.deal(self.players + [self.dealer], per_hand = 2)
         self.dealer.flip_first_card()
         for player in self.players:
             print(player)
@@ -116,31 +112,31 @@ class BJ_Game:
                     player.win()
             else:
                 for player in self.still_playing:
-                    if player.total> self.dealer.total:
+                    if player.total > self.dealer.total:
                         player.win()
-                    elif player.total<self.dealer.total:
+                    elif player.total < self.dealer.total:
                         player.lose()
                     else:
-                       player.push
+                        player.push()
         for player in self.players:
             player.clear()
         self.dealer.clear()
 def main():
-    print("\t\tДобро пожаловать в игру Блэк Джэк\n")
-    names=[]
-    number = games.ask_number("Сколько всего игроков?(1 - 7)", low = 1, high = 7)
-
+    print("\t\tWelcome to Black Jack!\n")
+    names = []
+    number = games.ask_number("How many players in game?(1 - 7): ", low = 1, high = 7)
     for i in range(number):
-        name = input("Ввведите имя игрока №" + str(i + 1) + " :")
+        name = input("Enter player name № " + str(i + 1) + ":")
         names.append(name)
     print()
-
     game = BJ_Game(names)
     again = None
     while again != "n":
         game.play()
-        again = games.ask_yes_no("\nХотите сыграть еще раз?")
-
-
+        again = games.ask_yes_no("\nWould you like to play again?")
 main()
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Secuirity
 
